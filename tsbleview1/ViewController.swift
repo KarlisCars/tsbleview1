7183d1c4acd89cd15074493383b026659f76f94b
Karlis Cars <karlis.cars@gmail.com>
2020-10-27 09:09:50 +0200

    Initial Commit

---
+++ tsbleview1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tsbleview1
+//
+//  Created by Karlis Cars on 27/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
